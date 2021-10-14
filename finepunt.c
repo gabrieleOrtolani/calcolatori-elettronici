5b5889808d82509c3ac1c005afa4ead1155b6a3b
gabrieleOrtolani <78178039+gabrieleOrtolani@users.noreply.github.com>
2021-10-14 16:58:00 +0200

    Add files via upload

---
+++ finepunt.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main()
+{   
+    int array[100], *minimo, size, c, posizione = 1;
+   
+    printf("inserisci dimensione array: \n");
+    scanf("%d", &size);
+   
+    printf("inserisci i %d interi: \n", size);
+   
+    for (c = 0; c < size; c++)
+        scanf("%d", &array[c]);
+  
+    
+    minimo= array;
+    
+    *minimo = *array;
+
+    for (c=0;c<size;c++)
+    	printf("%d - ",array[c]);
+    	
+    puts("\n");
+    
+    
+    for (c = 1; c < size; c++)
+    {
+        if (*(array+c) < *minimo)
+        {
+           *minimo = *(array+c);
+           posizione = c+1;
+        }
+    }
+   
+    printf("il minimo si trova nella posizione %d e vale %d\n", posizione, *minimo);
+    return 0;
+}
